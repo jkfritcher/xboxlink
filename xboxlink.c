42c290439e8f046478ea80df956740604711d5d0
Jason Fritcher <jkf@wolfnet.org>
2013-01-21 01:53:53 -0800

    Changed error logging to APUE logging framework Added daemonize function to have proxy run as a system daemon.

--- xboxlink.c
+++ xboxlink.c
@@ -1,6 +1,13 @@
 
+#include <sys/capability.h>
+#include <sys/resource.h>
+#include <sys/stat.h>
+#include <sys/time.h>
 #include <sys/types.h>
+
 #include <errno.h>
+#include <fcntl.h>
+#include <pthread.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -9,10 +16,9 @@
 #include <strings.h>
 #include <unistd.h>
 
-#include <pthread.h>
-
-#include <sys/capability.h>
 #include <pcap/pcap.h>
+
+#include "log.h"
 
 typedef struct {
     char *interface;
@@ -31,12 +37,13 @@ const uint8_t MAX_NUM_INTERFACES = 4;
 
 struct bpf_program xbl_bpf_filter;
 xbl_interface_t **xbl_interfaces;
-bool debug;
 
+int  log_to_stderr;
+bool debug;
 
 void usage(void)
 {
-    fprintf(stderr, "foobarbaz\n");
+    fputs("foobarbaz\n", stderr);
 }
 
 void handler(uint8_t *user, const struct pcap_pkthdr *h, const uint8_t *packet)
@@ -49,7 +56,7 @@ void handler(uint8_t *user, const struct pcap_pkthdr *h, const uint8_t *packet)
     xbl_interface_t *xit_out = (xit->idx == 0) ? xbl_interfaces[1] : xbl_interfaces[0];
     int rv = pcap_inject(xit_out->pcap, packet, h->caplen);
     if (rv == -1) {
-        fprintf(stderr, "pcap_inject failed: %s\n", pcap_geterr(xit_out->pcap));
+        log_msg("pcap_inject failed: %s", pcap_geterr(xit_out->pcap));
         return;
     }
 
@@ -64,13 +71,13 @@ bool xbl_pcap_init(xbl_interface_t *xit)
 
     xit->pcap = pcap_open_live(xit->interface, 65535, 1, 0, errbuf);
     if (xit->pcap == NULL) {
-        fprintf(stderr, "pcap_open_live failed: %s\n", errbuf);
+        log_msg("pcap_open_live failed: %s", errbuf);
         return false;
     }
 
     rv = pcap_setfilter(xit->pcap, &xbl_bpf_filter);
     if (rv == -1) {
-        fprintf(stderr, "pcap_setfilter failed: %s\n", pcap_geterr(xit->pcap));
+        log_msg("pcap_setfilter failed: %s", pcap_geterr(xit->pcap));
         pcap_close(xit->pcap);
         xit->pcap = NULL;
         return false;
@@ -78,7 +85,7 @@ bool xbl_pcap_init(xbl_interface_t *xit)
 
     rv = pcap_setdirection(xit->pcap, PCAP_D_IN);
     if (rv == -1) {
-        fprintf(stderr, "pcap_setdirection failed: %s\n", pcap_geterr(xit->pcap));
+        log_msg("pcap_setdirection failed: %s", pcap_geterr(xit->pcap));
         pcap_close(xit->pcap);
         xit->pcap = NULL;
         return false;
@@ -108,10 +115,12 @@ bool validate_interface_name(const char *int_name)
 {
     char errbuf[PCAP_ERRBUF_SIZE];
     pcap_t *p = pcap_open_live(int_name, 65535, 0, 0, errbuf);
-    if (p == NULL)
+    if (p == NULL) {
+        log_msg("pcap_open_live() failed: %s", errbuf);
         return 1;
-
+    }
     pcap_close(p);
+
     return 0;
 }
 
@@ -120,6 +129,8 @@ bool check_capabilities(void)
 {
     cap_flag_value_t cap_val;
     cap_t caps = cap_get_proc();
+    if (caps == NULL)
+        log_sys("cap_get_proc() failed");
     cap_get_flag(caps, CAP_NET_RAW, CAP_EFFECTIVE, &cap_val);
     cap_free(caps);
 
@@ -130,17 +141,52 @@ static
 bool prepare_bpf_filter(void)
 {
     pcap_t *p = pcap_open_dead(DLT_EN10MB, 65535);
-    if (p == NULL) {
-        fprintf(stderr, "pcap_open_dead() failed.");
-        return false;
-    }
     int rv = pcap_compile(p, &xbl_bpf_filter, "host 0.0.0.1", 1, PCAP_NETMASK_UNKNOWN);
-    if (rv < 0) {
-        fprintf(stderr, "pcap_compile() failed: %s\n", pcap_geterr(p));
-    }
+    if (rv < 0)
+        log_msg("pcap_compile() failed: %s", pcap_geterr(p));
     pcap_close(p);
 
     return (rv == 0);
+}
+
+static
+void daemonize(void)
+{
+    /* Fork into the background and detach from the controlling terminal */
+    pid_t pid;
+    if ((pid = fork()) < 0)
+        log_sys("fork() failed the first time");
+    if (pid > 0) /* parent */
+        exit(0);
+    setsid();
+    if ((pid = fork()) < 0)
+        log_sys("fork() failed the second time");
+    if (pid > 0) /* parent */
+        exit(0);
+
+    /* Change current directory to root */
+    if (chdir("/") < 0)
+        log_sys("chdir() to / failed");
+
+    /* Clear umask restrictions */
+    umask(0);
+
+    /* Redirect std{in,out,err} to /dev/null */
+    int fd = open("/dev/null", O_RDWR);
+    if (fd < 0)
+        log_sys("open() failed for /dev/null");
+
+    dup2(fd, 0); /* stdin */
+    dup2(fd, 1); /* stdout */
+    dup2(fd, 2); /* stderr */
+    close(fd);
+
+    struct rlimit rl;
+    getrlimit(RLIMIT_NOFILE, &rl);
+    if (rl.rlim_max == RLIM_INFINITY)
+        rl.rlim_max = 1024;
+    for (int i = 3; i < rl.rlim_max; i++)
+        close(i);
 }
 
 int main(int argc, char *argv[])
@@ -161,14 +207,10 @@ int main(int argc, char *argv[])
                 debug = true;
                 break;
             case 'i':
-                if (num_interfaces >= MAX_NUM_INTERFACES) {
-                    fprintf(stderr, "Too many interfaces specified, maximum is %d.\n", MAX_NUM_INTERFACES);
-                    exit(-1);
-                }
-                if (validate_interface_name(optarg) != 0) {
-                    fprintf(stderr, "%s does not appear to be a valid interface name.\n", optarg);
-                    exit(-1);
-                }
+                if (num_interfaces >= MAX_NUM_INTERFACES)
+                    err_quit("Too many interfaces specified, maximum is %d.", MAX_NUM_INTERFACES);
+                if (validate_interface_name(optarg) != 0)
+                    err_quit("%s does not appear to be a valid interface name.", optarg);
                 interfaces[num_interfaces] = optarg;
                 num_interfaces++;
                 break;
@@ -179,23 +221,21 @@ int main(int argc, char *argv[])
         }
     }
 
-    if (!prepare_bpf_filter()) {
-        fprintf(stderr, "Failed to compile bpf filter.\n");
-        exit(-1);
+    if (!debug) {
+        daemonize();
     }
 
+    if (!prepare_bpf_filter())
+        log_quit("Failed to compile bpf filter.");
+
     xbl_interfaces = malloc(sizeof(xbl_interface_t *) * (num_interfaces + 1));
-    if (xbl_interfaces == NULL) {
-        fprintf(stderr, "malloc() failed while allocating interfaces array: %s\n", strerror(errno));
-        exit(-1);
-    }
+    if (xbl_interfaces == NULL)
+        log_sys("malloc() failed while allocating interfaces array");
     bzero(xbl_interfaces, sizeof(xbl_interface_t *) * (num_interfaces + 1));
     for (int i = 0; i < num_interfaces; i++) {
         xbl_interfaces[i] = malloc(sizeof(xbl_interface_t));
-        if (xbl_interfaces[i] == NULL) {
-            fprintf(stderr, "malloc() failed while allocating xbl_interface struct: %s\n", strerror(errno));
-            exit(-1);
-        }
+        if (xbl_interfaces[i] == NULL)
+            log_sys("malloc() failed while allocating xbl_interface struct");
         bzero(xbl_interfaces[i], sizeof(xbl_interface_t));
 
         xbl_interfaces[i]->interface = interfaces[i];
